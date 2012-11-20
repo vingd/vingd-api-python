9e9fcf4803d35dc5b76ff9c2b24259c2178621d7
Radomir Stevanovic <radomir.stevanovic@knopso.com>
2012-11-20 17:21:50 +0100

    vingd package setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import vingd
+
+setup(
+    name=vingd.__name__,
+    version=vingd.__version__,
+    description=vingd.__doc__,
+    long_description=open('README.rst').read(),
+    author=vingd.__author__,
+    author_email=vingd.__author_email__,
+    url=vingd.__url__,
+    packages=[vingd.__name__],
+    package_dir={vingd.__name__: vingd.__name__},
+    license=vingd.__license__,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet',
+        'Topic :: Office/Business :: Financial'
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    )
+)
--- vingd/__init__.py
+++ vingd/__init__.py
@@ -1,11 +1,11 @@
-"""
-Vingd API interface client.
-"""
+"""Vingd API interface client."""
 
 __title__ = 'vingd-api-python'
 __version__ = '0.1'
 __author__ = 'Radomir Stevanovic'
+__author_email__ = 'radomir@vingd.com'
 __copyright__ = 'Copyright 2012 Vingd, Inc.'
 __license__ = 'MIT'
+__url__ = 'https://github.com/vingd/vingd-api-python'
 
 from .client import Vingd
